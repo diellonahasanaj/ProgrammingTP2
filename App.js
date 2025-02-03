7260bbdf287afec1807777d6a4e6e164859f4bdf
Diellona Hasanaj <124513703+diellonahasanaj@users.noreply.github.com>
2025-02-03 01:19:00 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
+
 import MainNavigator from './navigation/MainNavigator';
 
 export default function App() {
+
   return (
+
     <NavigationContainer>
+
       <MainNavigator />
+
     </NavigationContainer>
+
   );
+
 }
